da998bdca6c5295aed6ee8b60827ccf154f02b2b
David Silva <contatodavidmaia@gmail.com>
2023-03-18 19:49:48 -0300

    :refactor: adjust image error and app head

--- app.vue
+++ app.vue
@@ -1,4 +1,7 @@
 <template>
+  <Head>
+    <Title> David Silva </Title>
+  </Head>
   <div class="bg-secondary-900 min-h-full w-full">
     <NuxtLayout>
       <NuxtPage />
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="fixed top-0 flex w-full items-center justify-between p-8">
-    <img class="h-6" src="assets/logo.png" alt="David's Logo" />
+    <img class="h-6" src="../assets/logo.png" alt="David's Logo" />
 
     <button
       class="hover:bg-secondary-800 flex h-12 w-12 items-center justify-center rounded-full"
