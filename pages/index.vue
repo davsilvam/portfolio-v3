50fb1c628622c55d30acf8d77b2fada229414381
David Silva <contatodavidmaia@gmail.com>
2023-03-18 19:54:17 -0300

    refactor: adjust home title responsivity

--- pages/index.vue
+++ pages/index.vue
@@ -9,11 +9,13 @@
     <main class="flex h-full items-center justify-center">
       <div class="flex flex-col items-center">
         <h1 class="text-5xl font-bold">David Silva</h1>
-        <BaseOutlineTitle class="pt-2 text-3xl md:text-4xl">
+        <BaseOutlineTitle
+          class="pt-1 text-center text-3xl max-sm:w-[80%] md:text-4xl"
+        >
           Desenvolvedor Front-end
         </BaseOutlineTitle>
         <ul
-          class="flex items-center justify-center gap-6 pt-4 md:pt-6 font-[Poppins] text-lg md:gap-12"
+          class="flex items-center justify-center gap-6 pt-4 font-[Poppins] text-lg md:gap-12 md:pt-6"
         >
           <li>
             <h3>Atualizado</h3>
