1bf05a0d44b5ece95a74240d324db792acca5331
David Silva <contatodavidmaia@gmail.com>
2023-04-16 20:45:29 -0300

    style: change logo

--- src/components/App/Header/index.vue
+++ src/components/App/Header/index.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="bg-secondary-900 fixed top-0 z-10 flex w-full items-center justify-between p-8"
+    class="bg-secondary-900 fixed top-0 z-10 flex w-full items-center justify-between px-8 py-8 md:px-16 xl:px-24"
   >
     <nuxt-img class="h-6" src="/logo.png" alt="David's Logo" />
 
