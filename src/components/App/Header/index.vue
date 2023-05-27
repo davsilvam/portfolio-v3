8cb8e3e7e4dfa775cee2b990a0369a5d86beb681
David Silva <contatodavidmaia@gmail.com>
2023-05-27 19:25:41 -0300

    style: change logo

--- src/components/App/Header/index.vue
+++ src/components/App/Header/index.vue
@@ -2,7 +2,7 @@
   <header
     class="bg-night fixed top-0 z-10 flex w-full items-center justify-between px-8 py-8 md:px-16 xl:px-24"
   >
-    <img class="h-6" src="/logo.png" alt="David's Logo" />
+    <img class="h-6" src="/logo.svg" alt="David's Logo" />
 
     <AppNavigationContainer />
   </header>
